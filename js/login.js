c333a63f5397b2715d87221faf31cc3c3e7ce368
jhoswarf03 <jhoswarf03@gmail.com>
2020-08-26 11:59:30 -0400

    ahcer coincidir  lso cambiar en login con amrtes}

---
+++ js/login.js
@@ -0,0 +1,14 @@
+const mascota = {
+  nomre: 'Tom',
+  edad:'10',
+  vivo: true,
+  raza:['pelido','negro']
+}
+const nombremascota = mascota.nomre;
+const {edad,nomre} = mascota;
+console.log(edad);
+console.log(nomre);
+console.log(mascota);
+console.log(mascota.nomre);
+console.log(mascota.edad);
+console.log(mascota.vivo);
